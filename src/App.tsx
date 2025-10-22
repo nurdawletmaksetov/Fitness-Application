bd3ec8f22d8fb9cb54344409ef24f94f0e203383
Nurdawlet Maksetov <nurdawletmaksetov@gmail.com>
2025-10-23 00:02:43 +0500

    fully builded navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
+
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home")
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  })
 
   return (
     <>
       <div className='app bg-gray-20'>
-        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        <Navbar
+          isTopOfPage={isTopOfPage}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
       </div>
     </>
   )
---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { SelectedPage } from './types';
+
+type Props = {
+    children: React.ReactNode;
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const ActionButton = ({ children, setSelectedPage }: Props) => {
+    return (
+        <>
+            <AnchorLink
+                className='rounded-md bg-secondary-500 py-2 px-10 hover:bg-primary-500 hover:text-white'
+                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                href={`#${SelectedPage.ContactUs}`}
+            >
+                {children}
+            </AnchorLink>
+        </>
+    )
+}
+
+export default ActionButton
